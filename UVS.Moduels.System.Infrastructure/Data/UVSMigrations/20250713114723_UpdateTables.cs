2215b0391fea5b39c8aea05161977a70efa12aea
vendatta-1 <81135582@fci.s-mu.edu.eg>
2025-07-15 12:14:51 +0300

    migrations

---
+++ UVS.Moduels.System.Infrastructure/Data/UVSMigrations/20250713114723_UpdateTables.cs
@@ -0,0 +1,359 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace UVS.Modules.System.Infrastructure.Data.UVSMigrations
+{
+    /// <inheritdoc />
+    public partial class UpdateTables : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.EnsureSchema(
+                name: "system");
+
+            migrationBuilder.CreateTable(
+                name: "departments",
+                schema: "system",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    max_credit_hours_per_semester = table.Column<int>(type: "integer", nullable: false),
+                    head_of_department_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    created_by = table.Column<string>(type: "text", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_by = table.Column<string>(type: "text", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_by = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_departments", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "instructors",
+                schema: "system",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    full_name = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    department_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    created_by = table.Column<string>(type: "text", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_by = table.Column<string>(type: "text", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_by = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_instructors", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "students",
+                schema: "system",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    full_name = table.Column<string>(type: "text", nullable: false),
+                    national_id = table.Column<string>(type: "text", nullable: false),
+                    email = table.Column<string>(type: "text", nullable: false),
+                    phone = table.Column<string>(type: "text", nullable: false),
+                    date_of_birth = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    gender = table.Column<string>(type: "text", nullable: false),
+                    department_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    level = table.Column<int>(type: "integer", nullable: false),
+                    address_city = table.Column<string>(type: "text", nullable: false),
+                    address_zip_code = table.Column<string>(type: "text", nullable: false),
+                    address_town = table.Column<string>(type: "text", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    created_by = table.Column<string>(type: "text", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_by = table.Column<string>(type: "text", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_by = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_students", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "semesters",
+                schema: "system",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    start_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    end_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    is_current = table.Column<bool>(type: "boolean", nullable: false),
+                    department_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    academic_year = table.Column<int>(type: "integer", nullable: false),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    created_by = table.Column<string>(type: "text", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_by = table.Column<string>(type: "text", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_by = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_semesters", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_semesters_departments_department_id",
+                        column: x => x.department_id,
+                        principalSchema: "system",
+                        principalTable: "departments",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "courses",
+                schema: "system",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    code = table.Column<string>(type: "text", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    credit_hours = table.Column<int>(type: "integer", nullable: false),
+                    department_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    instructor_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    created_by = table.Column<string>(type: "text", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_by = table.Column<string>(type: "text", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_by = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_courses", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_courses_departments_department_id",
+                        column: x => x.department_id,
+                        principalSchema: "system",
+                        principalTable: "departments",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_courses_instructors_instructor_id",
+                        column: x => x.instructor_id,
+                        principalSchema: "system",
+                        principalTable: "instructors",
+                        principalColumn: "id");
+                });
+
+            migrationBuilder.CreateTable(
+                name: "office_hour",
+                schema: "system",
+                columns: table => new
+                {
+                    instructor_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    day = table.Column<int>(type: "integer", nullable: false),
+                    start_time = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    end_time = table.Column<TimeSpan>(type: "interval", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_office_hour", x => new { x.instructor_id, x.id });
+                    table.ForeignKey(
+                        name: "fk_office_hour_instructors_instructor_id",
+                        column: x => x.instructor_id,
+                        principalSchema: "system",
+                        principalTable: "instructors",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "enrollments",
+                schema: "system",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    student_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    course_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    enrollment_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    grade = table.Column<double>(type: "double precision", nullable: true),
+                    is_withdrawn = table.Column<bool>(type: "boolean", nullable: false),
+                    withdrawal_date = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    created_by = table.Column<string>(type: "text", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_by = table.Column<string>(type: "text", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_by = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_enrollments", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_enrollments_courses_course_id",
+                        column: x => x.course_id,
+                        principalSchema: "system",
+                        principalTable: "courses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_enrollments_students_student_id",
+                        column: x => x.student_id,
+                        principalSchema: "system",
+                        principalTable: "students",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "schedules",
+                schema: "system",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    course_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    day = table.Column<int>(type: "integer", nullable: false),
+                    start_time = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    end_time = table.Column<TimeSpan>(type: "interval", nullable: false),
+                    room = table.Column<string>(type: "text", nullable: true),
+                    created_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    created_by = table.Column<string>(type: "text", nullable: true),
+                    updated_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    updated_by = table.Column<string>(type: "text", nullable: true),
+                    deleted_at = table.Column<DateTime>(type: "timestamp with time zone", nullable: true),
+                    deleted_by = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_schedules", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_schedules_courses_course_id",
+                        column: x => x.course_id,
+                        principalSchema: "system",
+                        principalTable: "courses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "semester_course",
+                schema: "system",
+                columns: table => new
+                {
+                    semester_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    course_id = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_semester_course", x => new { x.course_id, x.semester_id });
+                    table.ForeignKey(
+                        name: "fk_semester_course_courses_course_id",
+                        column: x => x.course_id,
+                        principalSchema: "system",
+                        principalTable: "courses",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "fk_semester_course_semesters_semester_id",
+                        column: x => x.semester_id,
+                        principalSchema: "system",
+                        principalTable: "semesters",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_courses_department_id",
+                schema: "system",
+                table: "courses",
+                column: "department_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_courses_instructor_id",
+                schema: "system",
+                table: "courses",
+                column: "instructor_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_enrollments_course_id",
+                schema: "system",
+                table: "enrollments",
+                column: "course_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_enrollments_student_id",
+                schema: "system",
+                table: "enrollments",
+                column: "student_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_schedules_course_id",
+                schema: "system",
+                table: "schedules",
+                column: "course_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_semester_course_semester_id",
+                schema: "system",
+                table: "semester_course",
+                column: "semester_id");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_semesters_department_id",
+                schema: "system",
+                table: "semesters",
+                column: "department_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "enrollments",
+                schema: "system");
+
+            migrationBuilder.DropTable(
+                name: "office_hour",
+                schema: "system");
+
+            migrationBuilder.DropTable(
+                name: "schedules",
+                schema: "system");
+
+            migrationBuilder.DropTable(
+                name: "semester_course",
+                schema: "system");
+
+            migrationBuilder.DropTable(
+                name: "students",
+                schema: "system");
+
+            migrationBuilder.DropTable(
+                name: "courses",
+                schema: "system");
+
+            migrationBuilder.DropTable(
+                name: "semesters",
+                schema: "system");
+
+            migrationBuilder.DropTable(
+                name: "instructors",
+                schema: "system");
+
+            migrationBuilder.DropTable(
+                name: "departments",
+                schema: "system");
+        }
+    }
+}
---
+++ UVS.Modules.System.Integration/CreateStudentIntegrationEvent.cs
@@ -0,0 +1,28 @@
+﻿using UVS.Common.Application.EventBus;
+
+namespace UVS.Modules.System.Integration;
+
+public sealed class CreateStudentIntegrationEvent : IntegrationEvent
+{
+    public CreateStudentIntegrationEvent(
+        Guid id,
+        DateTime occurredOnUtc,
+        Guid studentId,
+        string firstName,
+        string lastName,
+        string email
+        ) : 
+        base(id, occurredOnUtc)
+    {
+        
+        FirstName = firstName;
+        LastName = lastName;
+        Email = email; 
+        StudentId = studentId;
+    }
+    public string FirstName { get; private set; }
+    public string LastName { get; private set; }
+    public string Email { get; private set; } 
+    public Guid StudentId { get; private set; }
+    
+}
